feeaa3bd7fb532da77834c6751d589324f22e804
srl23 <stephen.linfield@comcast.net>
2020-03-16 19:38:13 -0400

    typo let's see if it works now

--- controllers/postings.js
+++ controllers/postings.js
@@ -9,43 +9,44 @@ const Posting = require('../models/postings.js');
 //accounts will likely go nowhere
 router.get('/seed', (request, response) => {
 	Posting.create(
-	[
-		{
-			poster: "bill",
-			topic: "aoe 2 anybody?",
-			title: "Age of Empires 2",
-			message: "anybody up for a few rounds of aoe2? conquerors not required but it would be fun if I could be the aztecs. hit me up!",
-			contact: "bill@microsoft.com or bill#1342 on Discord"
-		},
-		{
-			poster: "mrlinux",
-			topic: "up for some Civ2, PBEM?",
-			title: "Sid Meier's Civilization II: Play the World!",
-			message: "I miss the good old days of Civ2 multiplayer. send me some mail or a discord PM",
-			contact: "lunis@linux.org or linus#9999 on discord"
-		},
-		{
-			poster: "jobs",
-			topic: "Back from the dead for some RoN!",
-			title: "Rise of Nations",
-			message: "Finally back on the internet, and it's great! Only thing that would make coming back to life better would be some Rise of Nations with friends. I get to be Russia though, gotta love that attrition!",
-			contact: "jobs on new iMessenger app" //sorry it does not exist :(
-		},
-		{
-			poster: "gaben",
-			topic: "HL2: DM would be great",
-			title: "Half-Life 2: Death Match",
-			message: "Let's play HL2: DM so I dont have to work on Half-Life 3. Please?",
-			contact: "thereisnohl2@gmail.com" //I dont know if this really is somebody's email I came up with something for this
-		},
-		{
-			poster: "ahennig",
-			topic: "Dungeon Siege anyone?",
-			title: "Dungeon Siege",
-			messages: "anybody up for some Dungeon Siege? I haven't played in quite some time it would be awesome to play this again. Thanks for reading!",
-			contact: "amy#3333 on Discord"
-		}
-	]
+		[
+			{
+				poster: "bill",
+				topic: "aoe 2 anybody?",
+				title: "Age of Empires 2",
+				message: "anybody up for a few rounds of aoe2? conquerors not required but it would be fun if I could be the aztecs. hit me up!",
+				contact: "bill@microsoft.com or bill#1342 on Discord"
+			},
+			{
+				poster: "mrlinux",
+				topic: "up for some Civ2, PBEM?",
+				title: "Sid Meier's Civilization II: Play the World!",
+				message: "I miss the good old days of Civ2 multiplayer. send me some mail or a discord PM",
+				contact: "lunis@linux.org or linus#9999 on discord"
+			},
+			{
+				poster: "jobs",
+				topic: "Back from the dead for some RoN!",
+				title: "Rise of Nations",
+				message: "Finally back on the internet, and it's great! Only thing that would make coming back to life better would be some Rise of Nations with friends. I get to be Russia though, gotta love that attrition!",
+				contact: "jobs on new iMessenger app" //sorry it does not exist :(
+			},
+			{
+				poster: "gaben",
+				topic: "HL2: DM would be great",
+				title: "Half-Life 2: Death Match",
+				message: "Let's play HL2: DM so I dont have to work on Half-Life 3. Please?",
+				contact: "thereisnohl2@gmail.com" //I dont know if this really is somebody's email I came up with something for this
+			},
+			{
+				poster: "ahennig",
+				topic: "Dungeon Siege anyone?",
+				title: "Dungeon Siege",
+				messages: "anybody up for some Dungeon Siege? I haven't played in quite some time it would be awesome to play this again. Thanks for reading!",
+				contact: "amy#3333 on Discord"
+			}
+		]
+	)
 });
 
 router.put('/:id', (request, response) => {
